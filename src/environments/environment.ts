cb80abfcd112eaf7f6c1342ffc8e70801efbd650
Exaud05 <snopyyoussef@gmail.com>
2023-11-13 19:19:23 +0100

    This is a commit

---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  firebase: {
+    projectId: 'facial-recognition-ab49f',
+    appId: '1:605287228826:web:1fee627860365fd520c623',
+    databaseURL: 'https://facial-recognition-ab49f-default-rtdb.europe-west1.firebasedatabase.app',
+    storageBucket: 'facial-recognition-ab49f.appspot.com',
+    apiKey: 'AIzaSyDdo87NoCRasKDRT2ZdjpeMtDgqMe9GlQo',
+    authDomain: 'facial-recognition-ab49f.firebaseapp.com',
+    messagingSenderId: '605287228826',
+  },
+  production: false
+};
